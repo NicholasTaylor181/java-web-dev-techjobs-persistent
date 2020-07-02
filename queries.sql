05b74a72af11ed5288b9841e66a993a7d86ced0a
Nick <nicholastaylor181@gmail.com>
2020-07-01 20:52:10 -0500

    finished part 2.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,10 @@
 ## Part 1: Test it with SQL
+SELECT * FROM techjobs.job;
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM techjobs.employer
+WHERE (location = "St. Louis");
 
 ## Part 3: Test it with SQL
 
