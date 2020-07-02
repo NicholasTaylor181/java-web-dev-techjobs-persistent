d27dfa543a2864ccb04d3975bd773e976f7af8c9
Nick <nicholastaylor181@gmail.com>
2020-07-02 18:40:32 -0500

    finished assignment 4.

--- queries.sql
+++ queries.sql
@@ -7,5 +7,10 @@ FROM techjobs.employer
 WHERE (location = "St. Louis");
 
 ## Part 3: Test it with SQL
+DROP TABLE `techjobs`.`job`;
 
 ## Part 4: Test it with SQL
+
+Select S.name, S.description from skill S
+inner join job_skills B on S.id = B.skills_id
+left outer join job J on B.jobs_id = J.id ORDER BY name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -10,6 +14,9 @@ public class Skill extends AbstractEntity {
     @NotBlank
     @Size(max=100)
     private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -23,5 +30,13 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
     }
 }
